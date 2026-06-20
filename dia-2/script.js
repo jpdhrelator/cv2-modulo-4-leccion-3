18d6fe754010d167da82fe43a14a2b3a299e8b22
Juan Pablo Duran <jpduranhe+001@gmail.com>
2026-06-19 22:13:11 -0400

    archivos

--- dia-2/script.js
+++ dia-2/script.js
@@ -1,28 +1,46 @@
-let salir = false;
+/*
+Crea un menu que se depliege en la consola
+que permite ingresar 2 semanas y en cada una 
+deben registrar 3 dias, luego del ingreso
+deben indicar cual fue el total de las ventas
+y el promedio de venta de las 2 semanas
+*/
+let ventas= [
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]
+];
 
-
-do {    
-    //
-    let inputUsuario=prompt(`Ingresa un opción del menu:
-            1) Sumar Numeros
-            2) Despedir
-            3) Salir
+let opcion="0";
+let ventasTotalizadas=0;
+let nroTotalVentas=0;
+do{
+    opcion= prompt(`
+            --- Menu ---
+            0. salir;
+            1. ingresar ventas
         `);
 
-    if(inputUsuario=="1"){
-        console.log("Hola Mundo"); 
-        
-        const nro1=prompt("Ingrese el primer nro");
-        const nro2=prompt("Ingrese el segundo nro");
+    if(opcion =="1"){
+        for (let i = 0; i < ventas.length; i++) {
+            alert(`Ingresando ventas Semana ${i+1}`)
+            for (let j = 0; j < ventas[i].length; j++) {
+                let ventaStr= prompt(`
+                    Ingrese Venta dia ${j+1}
+                    `);
+                let ventaInt= parseInt(ventaStr);
+                ventas[i][j]=ventaInt;
+                ventasTotalizadas+=ventaInt;
+                nroTotalVentas++;
+            }
+            
+        }
 
-        const resultado = parseInt(nro1) + parseInt(nro2);
-        alert(`El Resultado es ${resultado}`)
-    }else if(inputUsuario =="2"){
-        console.log("Chao Mundo Mundo");  
-    }else if(inputUsuario =="3"){
-        salir=true; 
-        console.log('Programa Terminado');        
-    }else{
-        alert('Ingrese un opcion válida')
-    }
-}while (!salir)
+        console.log(`El Total de Ventas fue ${ventasTotalizadas}`);
+        console.log(`El Promedio de Ventas fue ${ventasTotalizadas/nroTotalVentas}`);
+        
+
+
+    }    
+
+}while(opcion != "0");
